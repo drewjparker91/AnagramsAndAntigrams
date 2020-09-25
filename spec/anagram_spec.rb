c477d5e2085fb4b23999c6b8f66a3d02e4dc1c58
drewparker <drewjparker91@gmail.com>
2020-09-25 16:24:27 -0700

    receive good pass for test checking if false is received for two words that are not anagrams

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -9,7 +9,7 @@ describe ('AnagramCheck#is_anagram') do
 
   it ('will check if false is achieved in a case where two words are not anagrams')do
     ac = AnagramCheck.new('dog', 'fox')
-    expect(ac.is_anagram()).to(eq(true))
+    expect(ac.is_anagram()).to(eq(false))
   end
 
   it ('will check if two words are anagrams regardless of the case they are entered') do
