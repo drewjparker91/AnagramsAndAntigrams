fb2b6361a75fde44058b3e3922bcd9a99aad1d1b
drewparker <drewjparker91@gmail.com>
2020-09-25 10:46:15 -0700

    write test to check if two words are anagrams

---
+++ lib/anagram.rb
@@ -0,0 +1,13 @@
+class AnagramCheck
+ attr_accessor(:inputOne, :inputTwo)
+
+  def initialize (inputOne, inputTwo)
+    @inputOne = inputOne.downcase
+    @inputTwo = inputTwo.downcase
+  end
+
+  
+end
+
+
+
---
+++ spec/anagram_spec.rb
@@ -0,0 +1,27 @@
+require ('anagram')
+require ('rspec')
+
+describe ('AnagramCheck#anagram') do 
+  it ('will check if two words are anagrams') do 
+    ac = AnagramCheck.new('ruby', 'bury')
+    expect(ac.anagram_checker()).to(eq(true))
+  end
+end
+
+
+# will check if two words are not anagrams
+
+# will ignore capital letters while checking whether words are anagrams
+
+# will ignore spaces in inputted phrases 
+
+# will require words with vowels
+
+# will check if two words are antigrams
+
+# will ignore all inputted punctuation
+
+
+
+
+# will 
